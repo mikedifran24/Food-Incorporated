0572dccefdc4e4e61e2348c1d751de6f89220b81
Kevin Ellis <kevin.ellis@excella.com>
2019-02-07 16:25:07 -0500

    Working test

--- src/main/java/com/excella/foodinc/FoodItem.java
+++ src/main/java/com/excella/foodinc/FoodItem.java
@@ -3,9 +3,6 @@ package com.excella.foodinc;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Value;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
--- src/test/java/com/excella/foodinc/controllers/IngredientControllerTest.java
+++ src/test/java/com/excella/foodinc/controllers/IngredientControllerTest.java
@@ -3,17 +3,22 @@ package com.excella.foodinc.controllers;
 import com.excella.foodinc.common.FoodDto;
 import com.excella.foodinc.controllers.controllerInterface.IngredientControllerInterface;
 import com.excella.foodinc.services.FoodIncInterface;
+import static org.assertj.core.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import reactor.core.publisher.Mono;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.test.StepVerifier;
 
 
 import static org.mockito.Mockito.when;
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class IngredientControllerTest {
+
     @Autowired
     IngredientControllerInterface ingredientController;
     @MockBean
@@ -21,9 +26,16 @@ public class IngredientControllerTest {
 
     @Test
     public void IngredientController_pizzaOutput() {
-        Mono<FoodDto> expectedResult = Mono.just(new FoodDto("pizza", 400,4,10, 50));
+        FoodDto expectedDto = new FoodDto("pizza", 400,4,10, 50);
+        FoodDto otherDto = new FoodDto("pizza", 300,4,10, 50);
+
+        Mono<FoodDto> expectedResult = Mono.just(expectedDto);
         when(foodIncInterface.getNutrition("pizza")).thenReturn(expectedResult);
-        Mono<FoodDto> result = ingredientController.getNutritionData("pizza");
+
+
+        StepVerifier.create(ingredientController.getNutritionData("pizza"))
+                .assertNext(item -> assertThat(item).isEqualToComparingFieldByField(otherDto))
+                .verifyComplete();
     }
 }
 
